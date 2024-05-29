2d92498c91aa368744a5ca0394c655faa7d53d65
Nasteho Cali <nasteho1738@icloud.com>
2024-05-29 11:21:28 +0200

    Initial commit

---
+++ src/test/java/com/lexicon/jpa_workshop/JpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexicon.jpa_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
